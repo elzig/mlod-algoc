28d8ecb0ac2d29ff0738ee555b003564c889eb2c
elzig <dore.mathieu@protonmail.com>
2022-11-08 17:35:57 +0100

    ajout de la struct Musique

--- TP-04-liste-chainee/V2/mySpitofy.c
+++ TP-04-liste-chainee/V2/mySpitofy.c
@@ -4,6 +4,41 @@
 #include <string.h>
 
 
+typedef struct
+{
+    char* name;
+    char* artist;
+    char* album;
+    char* genre;
+    char* disc_number;
+    int track;
+    int number;
+    int year;
+} Musique;
+
+
+
+
+
+// définition des fonctions extern
+void afficheElement(Element e)
+{
+    
+}
+
+
+void detruireElement(Element e)
+{
+
+}
+
+
+bool equalsElement(Element e1, Element e2)
+{
+    
+}
+
+
 int main(void)
 {
     return EXIT_SUCCESS;

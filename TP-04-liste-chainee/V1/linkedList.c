3e1e683cb1042fcca13b5480b033a3469b39eeaf
elzig <dore.mathieu@protonmail.com>
2022-11-08 16:44:35 +0100

    ajout d'un main pour tester le code linkedList

--- TP-04-liste-chainee/V1/linkedList.c
+++ TP-04-liste-chainee/V1/linkedList.c
@@ -1,4 +1,4 @@
-#include "liste-chainee.h"
+#include "linkedList.h"
 #include <stdlib.h>
 #include <stdio.h>
 
---
+++ TP-04-liste-chainee/V1/linkedListOfString.c
@@ -0,0 +1,45 @@
+#include "linkedList.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+
+void afficheElement(Element e)
+{
+    printf("%s ", e);
+}
+
+
+void detruireElement(Element e)
+{}
+
+
+bool equalsElement(Element e1, Element e2)
+{
+    return(strcmp(e1, e2)==0);
+}
+
+
+int main(void)
+{
+    Liste l = NULL, p;
+    printf("estVide(l) = %s\n",estVide(l)?"TRUE":"FALSE");
+
+    l = ajoutTete("!",l);
+    l = ajoutTete("Mathieu",l);
+    l = ajoutTete("suis",l);
+    l = ajoutTete("je",l);
+    l = ajoutTete("Bonjour",l);
+
+    afficheListe_i(l);
+
+    p = cherche_r("je",l);
+	printf("cherche_r('je') : %s\n",estVide(p)?"[ERREUR] pas trouve":"trouve !!!");
+
+    printf("retirePremier_r('Mathieu')  : ");
+	l = retirePremier_r("Mathieu",l);
+	afficheListe_r(l);
+
+    detruire_r(l);
+
+    return EXIT_SUCCESS;
+}

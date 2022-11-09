656b402d68e5d0fe36e5bfc22cbf10bb042e02d9
elzig <dore.mathieu@protonmail.com>
2022-11-09 09:42:12 +0100

    implementation de la fonction afficheElement

--- TP-04-liste-chainee/V2/mySpitofy.c
+++ TP-04-liste-chainee/V2/mySpitofy.c
@@ -3,27 +3,29 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 typedef struct
 {
-    char* name;
-    char* artist;
-    char* album;
-    char* genre;
-    char* disc_number;
+    char *name;
+    char *artist;
+    char *album;
+    char *genre;
+    char *discNumber;
     int track;
     int number;
     int year;
 } Musique;
 
 
-
-
-
 // définition des fonctions extern
 void afficheElement(Element e)
 {
-    
+    printf("%s ", ((Musique *)e)->name);
+    printf("%s ", ((Musique *)e)->artist);
+    printf("%s ", ((Musique *)e)->album);
+    printf("%s ", ((Musique *)e)->genre);
+    printf("%i ", ((Musique *)e)->discNumber);
+    printf("%i ", ((Musique *)e)->number);
+    printf("%i ", ((Musique *)e)->year);
 }
 
 
@@ -35,14 +37,13 @@ void detruireElement(Element e)
 
 bool equalsElement(Element e1, Element e2)
 {
-    
+
 }
 
 
 int main(void)
 {
     // Ouverture du fichier
-    
 
     return EXIT_SUCCESS;
 }
